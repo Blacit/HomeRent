f714fe4c1966c0a022019ca6b7a87e9c5987b144
Blacit <vgilenko@bk.ru>
2020-10-01 15:59:53 +0300

    fix

--- src/main/java/task/homerent/repository/ContractRepository.java
+++ src/main/java/task/homerent/repository/ContractRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import task.homerent.model.Contract;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+    findContractByHouseIdAndEnd_dateAfter(String houseId LocalDate from);
 }
